1404c2349cb4564f70e3ad7ede79008c8da7fde7
earlfunk21 <60378076+earlfunk21@users.noreply.github.com>
2022-11-10 14:07:20 +0800

    Fixing bugs

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
+from datetime import datetime
 from flask import Flask
 from config import DevelopmentConfig, ProductionConfig
 from flask_session import Session
 
 # Extensions
-from app.models import db
+from app.models import Expenses, User, UserDetails, db
 from app.extensions.imagekit import FlaskImageKit
 from .extensions.flask_captcha import FlaskCaptcha
 from flask_migrate import Migrate
+
+from openpyxl import load_workbook
 
 
 captcha = FlaskCaptcha()
@@ -25,6 +28,62 @@ def create_app(config=ProductionConfig):
 
     # Context Processor
     context_processor(app)
+
+
+    # Initialization
+    with app.app_context():
+        db.create_all()
+
+        users = []
+    
+        admin = User(
+            username=app.config.get("ADMIN_USERNAME"),
+            password=app.config.get("ADMIN_PASSWORD"),
+            is_admin=True,
+        )
+        users.append(admin)
+    
+        # Load Users
+        workbook = load_workbook("Arknet.xlsx")
+        sheet = workbook.active
+        for last_name, first_name in sheet['K3': 'L34']:
+            first_name = str(first_name.value).strip(' ').lower()
+            last_name = str(last_name.value).strip(' ').lower()
+            username = f"{last_name}.{first_name}"
+            password = f"{last_name}123"
+            user_details = UserDetails(
+                first_name=first_name,
+                last_name=last_name,
+                address="Bougainvillea Village"
+            )
+            user = User(
+                username=username,
+                password=password,
+                user_details=user_details,
+                is_admin=False
+            )
+            users.append(user)
+            
+        db.session.add_all(users)
+    
+        expenseses = []
+        # Load Expenses
+        for date, amount, name in sheet['A10': 'C36']:
+            date = str(date.value).replace('Released ', '')
+            date = datetime.strptime(date, '%m/%d/%Y')
+    
+            expenses = Expenses(
+                name=name.value,
+                cost = float(amount.value),
+                created_on=date,
+                user=admin
+            )
+            expenseses.append(expenses)
+    
+        db.session.add_all(expenseses)
+        db.session.commit()
+    
+        print("Successfully initialized")
 
     return app
 
