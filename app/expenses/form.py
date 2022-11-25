9f634e46e4eaf89e9c365c16e19f89f812192083
earlfunk21 <60378076+earlfunk21@users.noreply.github.com>
2022-11-25 21:39:54 +0800

    IntegerField to DecimalField

--- app/expenses/form.py
+++ app/expenses/form.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, FileField, PasswordField, validators, StringField
+from wtforms import DecimalField, FileField, PasswordField, validators, StringField
 from flask_wtf.file import FileAllowed
 from flask import current_app
 
 
 class ExpensesForm(FlaskForm):
     name = StringField("Name of Expenses", validators=[validators.InputRequired()])
-    cost = IntegerField("Cost", default=0)
+    cost = DecimalField("Cost", places=2)
     receipt = FileField('Proof of Payment', validators=[FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
     admin_key = PasswordField("Secret Key", validators=[validators.InputRequired()])
     
