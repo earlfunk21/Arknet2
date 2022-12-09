6d58d65375dde9fbefb66d1c50055e8ca5639b55
developer <dev@ip-172-31-22-46.us-east-2.compute.internal>
2022-12-09 01:30:22 +0000

    Changed send_email.py script

--- send_email.py
+++ send_email.py
@@ -5,53 +5,21 @@ import boto3
 from botocore.exceptions import ClientError
 
 
-'''
-def send_email(receiver_email, subject, html):
-    sender_email = "earlfunk21@gmail.com"
-    password = "qmouojxbwvnxtnhk"
-
-    message = MIMEMultipart("alternative")
-    message["Subject"] = subject
-    message["From"] = sender_email
-    message["To"] = receiver_email
-
-    # Create the plain-text and HTML version of your message
-    text = """\
-    Hi,
-    How are you?
-    app.arknetfiber.com"""
-    
-
-    message.attach(MIMEText(text, "plain"))
-    message.attach(MIMEText(html, "html"))
-
-    # Create secure connection with server and send email
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(
-            sender_email, receiver_email, message.as_string()
-        )
-'''
-
-
 def send_email(receiver_email, subject, html):
 
-    SENDER = "earlfunk21@gmail.com"
+    SENDER = "Arknet Billing <billing@arknetfiber.com>"
     RECIPIENT = receiver_email
-    CONFIGURATION_SET = "ConfigSet"
-    AWS_REGION = "us-west-2"
+    AWS_REGION = "us-east-2"
     SUBJECT = subject
-    BODY_TEXT = "Welcome to Arknet Fiber\r\n"
     BODY_HTML = html
     CHARSET = "UTF-8"
     client = boto3.client("ses", region_name=AWS_REGION)
     try:
-        # Provide the contents of the email.
         response = client.send_email(
             Destination={
                 "ToAddresses": [
                     RECIPIENT,
+					"odracirb2@gmail.com",
                 ],
             },
             Message={
@@ -60,10 +28,6 @@ def send_email(receiver_email, subject, html):
                         "Charset": CHARSET,
                         "Data": BODY_HTML,
                     },
-                    "Text": {
-                        "Charset": CHARSET,
-                        "Data": BODY_TEXT,
-                    },
                 },
                 "Subject": {
                     "Charset": CHARSET,
@@ -71,12 +35,11 @@ def send_email(receiver_email, subject, html):
                 },
             },
             Source=SENDER,
-            # If you are not using a configuration set, comment or delete the
-            # following line
-            ConfigurationSetName=CONFIGURATION_SET,
         )
     except ClientError as e:
         print(e.response["Error"]["Message"])
     else:
         print("Email sent! Message ID:"),
-        print(response["MessageId"])
+        print(response["MessageId"]
+)
+
